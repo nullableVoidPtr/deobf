b617b11662d39553abdd0ae89790c4c66375863e
nullableVoidPtr <30564701+nullableVoidPtr@users.noreply.github.com>
2020-05-24 18:30:19 +0800

    Restructure

--- bin/javascript-obfuscator.js
+++ bin/javascript-obfuscator.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 const esprima = require('esprima');
-const {yargsOptions, deobfuscateESTree} = require('./lib');
+const JavascriptObfuscatorTarget = require('../lib/targets/JavascriptObfuscatorTarget.js');
 const escodegen = require('escodegen');
 const fs = require('fs');
 
 const argv = require('yargs')
 	.usage('$0 <source> [destination]', 'deobfuscate a Javascript Obfuscator obfuscated file',
 		(yargs) => {
-			yargs.options(yargsOptions)
+			yargs.options(JavascriptObfuscatorTarget.yargsOptions)
 			.positional('source', {
 				type: 'string'
 			}).positional('destination', {
@@ -18,7 +18,7 @@ const argv = require('yargs')
 	).argv;
 
 let tree = esprima.parse(fs.readFileSync(argv.source).toString());
-deobfuscateESTree(tree, argv);
+JavascriptObfuscatorTarget.deobfuscateESTree(tree, argv);
 let deobfuscatedSource = escodegen.generate(tree);
 if (typeof argv.destination === 'undefined') {
 	console.log(deobfuscatedSource);
