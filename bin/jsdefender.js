74735a15e2097cfcb486c7d6505d213d9f98ea90
nullableVoidPtr <30564701+nullableVoidPtr@users.noreply.github.com>
2020-06-01 19:19:03 +0800

    Begin JSDefenderTarget

--- bin/jsdefender.js
+++ bin/jsdefender.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 const esprima = require('esprima');
-const TerserTarget = require('../lib/targets/TerserTarget.js');
+const JSDefenderTarget = require('../lib/targets/JSDefenderTarget.js');
 const escodegen = require('escodegen');
 const fs = require('fs');
 
 const argv = require('yargs')
 	.usage('$0 <source> [destination]', 'deobfuscate a Terser minified file',
 		(yargs) => {
-			yargs.options(TerserTarget.yargsOptions)
+			yargs.options(JSDefenderTarget.yargsOptions)
 			.positional('source', {
 				type: 'string'
 			}).positional('destination', {
@@ -18,7 +18,7 @@ const argv = require('yargs')
 	).argv;
 
 let tree = esprima.parse(fs.readFileSync(argv.source).toString());
-TerserTarget.deobfuscateESTree(tree, argv);
+JSDefenderTarget.deobfuscateESTree(tree, argv);
 let deobfuscatedSource = escodegen.generate(tree);
 if (typeof argv.destination === 'undefined') {
 	console.log(deobfuscatedSource);
