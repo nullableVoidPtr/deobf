bff0b66c5e84eb30821c8e9fc69d004b47308c58
nullableVoidPtr <30564701+nullableVoidPtr@users.noreply.github.com>
2022-02-11 04:32:37 -0500

    add back jsdefender

---
+++ bin/jsdefender.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import cli from '../src/cli.js';
+
+cli('JSDefender', 'deobfuscate a file obfuscated by JSDefender');
--- bin/yamu.js
+++ bin/yamu.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import cli from '../src/cli.js';
 
-cli('YamuObfuscator', 'deobfuscate a file obfuscated by Javascript Obfuscator');
+cli('YamuObfuscator', 'deobfuscate a file obfuscated by Yamu Obfuscator');
